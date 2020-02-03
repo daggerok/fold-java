e8b5b7830e5dce3ad300796ee53216d076a70bdb
Maksim Kostromin <daggerok@gmail.com>
2020-02-04 00:46:55 +0200

    I CAN HAZ PYTHON, I CAN HAZ INDENTS (C) whatthecommit.com

--- src/test/kotlin/com/github/daggerok/distributed/TODO_Distributed_Coordination_Tests.java
+++ src/test/kotlin/com/github/daggerok/distributed/TODO_Distributed_Coordination_Tests.java
@@ -5,16 +5,12 @@ import org.junit.jupiter.api.Test;
 class TODO_Distributed_Coordination_Tests {
 
     @Test void leader_election() { }
-
     @Test void synchronization_distributed_semaphore() { }
-
     @Test void synchronization_distributed_barrier() { }
-
     @Test void liveness_service_discovery() { }
-
     @Test void distributed_microservices_configuration() { }
 
-    class Challenges {
+    static class Challenges {
         void concurrency_problems() {}
         void distributed_failures() {}
         void consistency_issues() {}
