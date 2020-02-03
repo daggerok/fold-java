0d4f28653c51fcd69cf6f99a013c8e74ca0bd8d1
Maksim Kostromin <daggerok@gmail.com>
2020-02-04 00:44:22 +0200

    jobs... steve jobs (C) whatthecommit.com

---
+++ src/test/kotlin/com/github/daggerok/distributed/TODO_Distributed_Coordination_Tests.java
@@ -0,0 +1,16 @@
+package com.github.daggerok.distributed;
+
+import org.junit.jupiter.api.Test;
+
+class TODO_Distributed_Coordination_Tests {
+
+    @Test void leader_election() { }
+
+    @Test void synchronization_distributed_semaphore() { }
+
+    @Test void synchronization_distributed_barrier() { }
+
+    @Test void liveness_service_discovery() { }
+
+    @Test void distributed_microservices_configuration() { }
+}
